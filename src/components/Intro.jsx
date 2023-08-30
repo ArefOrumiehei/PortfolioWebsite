6641b81d6c758afdfd2870c6028a69087a29991f
Aref Orumiehei <areforumiehei@gmail.com>
2023-08-30 18:24:39 +0330

    create intro section and use ityped package

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,10 +1,44 @@
+//Styles
 import '../scss/Intro.scss';
+
+//Photos
+import Picture from '../assets/images/ryangoslingNoBG.jpg'
+
+//Icons
+import { IconChevronDown } from '@tabler/icons-react';
+import { useEffect, useRef } from 'react';
+import { init } from 'ityped';
 
 
 const Intro = () => {
+
+    const textRef = useRef()
+
+    useEffect(() => {
+        init(textRef.current, 
+            {showCursor: true,
+            backDelay: 1500,
+            backSpeed: 60,
+            strings: ['Front-end Developer', 'Designer' , 'Content Creator' ] })
+    }, [])
+
     return (
         <div className='intro' id='intro'>
-            intro
+            <div className="left">
+                <div className="imgContainer">
+                    <img src={Picture} alt="" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi there, I`m</h2>
+                    <h1>Aref Orumiehei</h1>
+                    <h3>Freelance <span ref={textRef}></span></h3>
+                </div>
+                <a href="#portfolio" className='downBtn'>
+                    <IconChevronDown size={65} stroke={1}/>
+                </a>
+            </div>
         </div>
     );
 };
