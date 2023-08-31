15fb62fa9b31f3cb6f56daad5a099740d796064e
Aref Orumiehei <areforumiehei@gmail.com>
2023-08-31 15:58:35 +0330

    create Contact section and make website responsive

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,37 @@
+//Styles
 import '../scss/Contact.scss';
 
+//Image
+import contactImg from '../assets/images/website-content-sections.png'
+import { useState } from 'react';
+
 const Contact = () => {
+
+    const [message , setMessage] = useState(false)
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        setMessage(true)
+
+        setTimeout(() => {
+            setMessage(false)
+        }, 3000)
+    }
+
     return (
         <div className='contact' id='contact'>
-            contact
+            <div className="left">
+                <img src={contactImg} alt="" />
+            </div>
+            <div className="right">
+                <h2>Contact.</h2>
+                <form onSubmit={submitHandler}>
+                    <input type="text" id="text" placeholder='Email'/>
+                    <textarea placeholder='Message'></textarea>
+                    <button type="submit">Send</button>
+                    {message && <span>Thanks for your message.</span>}
+                </form>
+            </div>
         </div>
     );
 };
